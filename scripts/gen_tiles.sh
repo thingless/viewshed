00e56d287fc45798252d8f31a95e66c673334347
Richard <rpklafter@yahoo.com>
2015-10-15 18:33:56 -0700

    bug fixes

--- scripts/gen_tiles.sh
+++ scripts/gen_tiles.sh
@@ -3,7 +3,7 @@ set -o errexit
 set -o nounset
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if [ "$#" -ne 1 ] ; then
+if [ "$#" -eq 0 ] ; then
   echo "Usage: gen_tiles.sh tif_files"
   exit 1
 fi
@@ -13,4 +13,4 @@ gdalbuildvrt ./merged.vrt $1
 #(Math.cos(40 * Math.PI/180) * 2 * Math.PI * 6378137) / (256 * Math.pow(2, 12))
 ${__dir}/gdal2tiles.py -w none -n -f tiff -p mercator -z 12 -v ./merged.vrt tiles
 #need to strip 2nd band and compress
-find ./tiles/ -name "*.tiff" | parallel 'echo gdal_translate -co "PREDICTOR=2" -co "TILED=YES" -co "COMPRESS=DEFLATE" -b 1 {} {.}; mv {.} {}'
+#find ./tiles/ -name "*.tiff" | parallel 'echo gdal_translate -co "PREDICTOR=2" -co "TILED=YES" -co "COMPRESS=DEFLATE" -b 1 {} {.}; mv {.} {}'
