bbbfe967012ede876bb2360d62ec1db434465929
Richard <rpklafter@yahoo.com>
2015-10-15 21:33:43 -0700

    added note file

--- scripts/gen_tiles.sh
+++ scripts/gen_tiles.sh
@@ -8,7 +8,11 @@ if [ "$#" -eq 0 ] ; then
   exit 1
 fi
 
-gdalbuildvrt ./merged.vrt $1
+echo "creating tile pyramid for..."
+echo $@
+sleep 3
+
+gdalbuildvrt ./merged.vrt $@
 #Zoom level 12 is 29.277080383193702 m / pixel near SF
 #(Math.cos(40 * Math.PI/180) * 2 * Math.PI * 6378137) / (256 * Math.pow(2, 12))
 ${__dir}/gdal2tiles.py -w none -n -f tiff -p mercator -z 12 -v ./merged.vrt tiles
