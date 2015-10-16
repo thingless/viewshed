c5502a7557378010880f11b12f49103b7d9383e8
Richard <rpklafter@yahoo.com>
2015-10-15 18:26:39 -0700

    mabey finished script?

--- scripts/gen_tiles.sh
+++ scripts/gen_tiles.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
+set -o errexit
+set -o nounset
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if [ "$#" -ne 1 ] ; then
+  echo "Usage: gen_tiles.sh tif_files"
+  exit 1
+fi
+
 gdalbuildvrt ./merged.vrt $1
 #Zoom level 12 is 29.277080383193702 m / pixel near SF
 #(Math.cos(40 * Math.PI/180) * 2 * Math.PI * 6378137) / (256 * Math.pow(2, 12))
-~/projects/shed/scripts/gdal2tiles.py -w none -n -f tiff -p mercator -z 12 -v tiles
+${__dir}/gdal2tiles.py -w none -n -f tiff -p mercator -z 12 -v ./merged.vrt tiles
+#need to strip 2nd band and compress
+find ./tiles/ -name "*.tiff" | parallel 'echo gdal_translate -co "PREDICTOR=2" -co "TILED=YES" -co "COMPRESS=DEFLATE" -b 1 {} {.}; mv {.} {}'
