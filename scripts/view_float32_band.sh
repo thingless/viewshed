d6c3ed1f4a9580d26b4a8ab06b66621285708360
Richard <rpklafter@yahoo.com>
2015-10-15 17:32:59 -0700

    bug fixes

--- scripts/view_float32_band.sh
+++ scripts/view_float32_band.sh
@@ -2,12 +2,12 @@
 set -o errexit
 set -o nounset
 if [ "$#" -ne 2 ] ; then
-  echo "Usage: view_band.sh band_number input_file"
+  echo "Usage: view_float32_band.sh band_number input_file"
   exit 1
 fi
-INPUT_FILE=$(readlink -f $2)
+INPUT_FILE=$(perl -e 'use Cwd "abs_path";print abs_path(shift)' $2)
 #make temp working directory
-TEMP_DIR=$(mktemp -d)
+TEMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t 'tmp_view_float32_band')
 function finish {
   rm -r $TEMP_DIR #cleanup
 }
@@ -17,6 +17,8 @@ cd $TEMP_DIR
 OUTPUT_FILE="${INPUT_FILE%.*}.png"
 printf "Reading $INPUT_FILE and creating $OUTPUT_FILE from band 1\n\n"
 cp $INPUT_FILE ./input_file
+NODATA=$(gdalinfo $INPUT_FILE | grep -i nodata | awk "NR==$1" | awk -F'=' '{print $2}')
+
 #extract band
 gdal_translate -b $1 input_file band
 #color the band
@@ -25,6 +27,7 @@ echo "75% 235 220 175" >> color_relief.txt
 echo "50% 190 185 135" >> color_relief.txt
 echo "25% 240 250 150" >> color_relief.txt
 echo "0% 50 180 50" >> color_relief.txt
+echo "$NODATA 255 0 0" >> color_relief.txt
 gdaldem color-relief band color_relief.txt band_color
 #convert and return
 gdal_translate -of PNG band_color $OUTPUT_FILE
