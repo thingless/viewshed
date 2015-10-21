782e604571c69ce569871281583c09fb16f7d3a1
Richard Klafter <rpklafter@yahoo.com>
2015-10-20 19:51:35 -0700

    stupid server stub

--- server.py
+++ server.py
@@ -2,16 +2,18 @@ import tornado.ioloop
 import tornado.web
 from tornado import gen
 
-class TileHandler(tornado.web.RequestHandler):
+TILE_HOST = "http://127.0.0.1"
+
+class ElevationHandler(tornado.web.RequestHandler):
   @gen.coroutine
   def get(self, lat, lng):
     http_client = AsyncHTTPClient()
-    response = yield http_client.fetch("http://example.com")
+    response = yield http_client.fetch(TILE_HOST+"")
     do_something_with_response(response)
     self.render("template.html")
 
 application = tornado.web.Application([
-  (r"/", MainHandler),
+  (r"/(\d+\.?\d*)/(\d+\.?\d*)", ElevationHandler),
 ])
 
 if __name__ == "__main__":
