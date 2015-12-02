05be9a2389cbd93696aaf73ee0eee517179c3b97
Eric Swanson <eswanson@alloscomp.com>
2015-12-01 19:59:28 -0800

    Server changes

--- server.py
+++ server.py
@@ -1,96 +1,100 @@
 import tornado.ioloop
 import tornado.web
+from tornado.httpclient import AsyncHTTPClient
 from tornado import gen
 from PIL import Image
+import dict2xml
 import math
 import tornado
 
-TILE_HOST = "http://127.0.0.1"
+TILE_HOST = "http://127.0.0.1:8080"
 
 class ApiHandler(tornado.web.RequestHandler):
-  def get_format(self):
-    format = self.get_argument('format', None)
-    if not format:
-        accept = self.request.headers.get('Accept')
-        if accept:
-            if 'javascript' in accept:
-                format = 'jsonp'
-            elif 'json' in accept:
-                format = 'json'
-            elif 'xml' in accept:
-                format = 'xml'
-    return format or 'json'
+    def get_format(self):
+        format = self.get_argument('format', None)
+        if not format:
+                accept = self.request.headers.get('Accept')
+                if accept:
+                        if 'javascript' in accept:
+                                format = 'jsonp'
+                        elif 'json' in accept:
+                                format = 'json'
+                        elif 'xml' in accept:
+                                format = 'xml'
+        return format or 'json'
 
-  def write_response(self, obj, nofail=False):
-    format = self.get_format()
-    if format == 'json':
-        self.set_header("Content-Type", "application/javascript")
-        self.write(json.dumps(obj))
-    elif format == 'jsonp':
-        self.set_header("Content-Type", "application/javascript")
-        callback = self.get_argument('callback', 'callback')
-        self.write('%s(%s);'%(callback, json.dumps(obj)))
-    elif format == 'xml':
-        self.set_header("Content-Type", "application/xml")
-        self.write('<response>%s</response>'%dict2xml.dict2xml(obj))
-    elif nofail:
-        self.write(json.dumps(obj))
-    else:
-        raise tornado.web.HTTPError(400, 'Unknown response format requested: %s'%format)
+    def write_response(self, obj, nofail=False):
+        format = self.get_format()
+        if format == 'json':
+                self.set_header("Content-Type", "application/javascript")
+                self.write(json.dumps(obj))
+        elif format == 'jsonp':
+                self.set_header("Content-Type", "application/javascript")
+                callback = self.get_argument('callback', 'callback')
+                self.write('%s(%s);'%(callback, json.dumps(obj)))
+        elif format == 'xml':
+                self.set_header("Content-Type", "application/xml")
+                self.write('<response>%s</response>'%dict2xml.dict2xml(obj))
+        elif nofail:
+                self.write(json.dumps(obj))
+        else:
+                raise tornado.web.HTTPError(400, 'Unknown response format requested: %s'%format)
 
-  def write_error(self, status_code, exc_info=None, **kwargs):
-      errortext = 'Internal error'
-      if exc_info:
-          errortext = getattr(exc_info[1], 'log_message', errortext)
+    def write_error(self, status_code, exc_info=None, **kwargs):
+            errortext = 'Internal error'
+            if exc_info:
+                    errortext = getattr(exc_info[1], 'log_message', errortext)
 
-      self.write_response({'status' : 'error',
-                           'code' : status_code,
-                           'reason' : errortext},
-                          nofail=True)
+            self.write_response({'status' : 'error',
+                                                     'code' : status_code,
+                                                     'reason' : errortext},
+                                                    nofail=True)
 
 class CoordSystem(object):
-  R = 6378137
-  MAX_LATITUDE = 85.0511287798
-  @classmethod
-  def latlng_to_pixel(cls, latlng):
-    d = math.pi / 180.0,
-    max = self.MAX_LATITUDE,
-    lat = math.max(math.min(max, latlng[0]), -max),
-    sin = math.sin(lat * d);
-    return (self.R * latlng[1] * d, self.R * math.log((1.0 + sin) / (1.0 - sin)) / 2.0)
-  @classmethod
-  def latlng_to_tile(cls, latlng):
-    r = cls.latlng_to_pixel(latlng)
-    return (r[0]/256.0, r[1]/256.0)
-  @classmethod
-  def pixel_to_latlng(cls, point):
-    d = 180.0 / math.pi
-    return ((2.0 * math.atan(math.exp(point[1] / self.R)) - (math.pi / 2.0)) * d,
-      point[0] * d / self.R))
-
+    @classmethod
+    def latlng_to_pixel(cls, latlng, zoom=12):
+        lat, lng = latlng
+        lat *= math.pi / 180.0
+        lng *= math.pi / 180.0
+        x = 128.0 / math.pi * 2**zoom * (lng + math.pi)
+        y = 128.0 / math.pi * 2**zoom * (math.pi - math.log(math.tan(math.pi / 4.0 + lat / 2.0)))
+        print latlng, '->', (x,y), '->', (x//256,y//256)
+        return round(x), round(y)
+    @classmethod
+    def latlng_to_tile(cls, latlng, zoom=12):
+        r = cls.latlng_to_pixel(latlng, zoom)
+        return (int(r[0]//256), int(r[1]//256))
+    @classmethod
+    def pixel_to_latlng(cls, point, zoom=12):
+        x, y = point
+        lat = (4.0 * math.atan(math.exp(math.pi - y * math.pi / (128.0 * 2**zoom))) - math.pi) / 2.0
+        lng = x * math.pi / (128.0 * 2**zoom) - math.pi
+        lat *= 180.0 / math.pi
+        lng *= 180.0 / math.pi
+        return lat, lng
 
 class ElevationHandler(ApiHandler):
-  @gen.coroutine
-  def get(self, lat, lng):
-    try:
-      latlng = map(float, (lat, lat))
-    except Exception:
-      raise tornado.web.HTTPError(400)
-    tile = CoordSystem.latlng_to_tile(latlng)
-    http_client = AsyncHTTPClient()
-    response = yield http_client.fetch(TILE_HOST+"/{z}/{x}/{y}.tif".format(z=12, x=tile[0], y=tile[1]))
-    if response.code != 200:
-      raise tornado.web.HTTPError(response.code)
-    im = Image.open(response.buffer)
-    pixel = [int(round(i))%255 for i in pixel = CoordSystem.latlng_to_pixel(latlng)]
-    value = im[pixel[0], pixel[1]]
-    im = None
-    self.write_response({"value":im})
+    @gen.coroutine
+    def get(self, lat, lng):
+        try:
+            latlng = map(float, (lat, lng))
+        except Exception:
+            raise tornado.web.HTTPError(400)
+        tile = CoordSystem.latlng_to_tile(latlng)
+        http_client = AsyncHTTPClient()
+        response = yield http_client.fetch(TILE_HOST+"/{z}/{x}/{y}.tiff".format(z=12, x=tile[0], y=tile[1]))
+        if response.code != 200:
+            raise tornado.web.HTTPError(response.code)
+        im = Image.open(response.buffer)
+        pixel = [int(round(i))%255 for i in CoordSystem.latlng_to_pixel(latlng)]
+        value = im.getpixel((pixel[0], pixel[1]))
+        im = None
+        self.write_response({"value":im})
 
 application = tornado.web.Application([
-  (r"elevation/(\d+\.?\d*)/(\d+\.?\d*)", ElevationHandler),
+    (r"/elevation/(-?\d+\.?\d*)/(-?\d+\.?\d*)", ElevationHandler),
 ])
 
 if __name__ == "__main__":
-  application.listen(8888)
-  tornado.ioloop.IOLoop.current().start()
+    application.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
