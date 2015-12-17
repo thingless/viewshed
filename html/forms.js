991b5338e6c3b100363273b44cfe6eecee53e458
Richard Klafter <richard@dronedeploy.com>
2015-12-16 17:22:30 -0800

    added some form stuff

---
+++ html/forms.js
@@ -0,0 +1,89 @@
+var React = require('react');
+var Formsy = require('formsy-react');
+var _ = require('lodash');
+var Geosuggest = require('react-geosuggest');
+
+Formsy.addValidationRule('isGreaterThanOrEqual', function (values, value, number) {
+  if(!value && value!==0){ return true; }
+  return parseFloat(value) >= parseFloat(number);
+});
+Formsy.addValidationRule('isLessThanOrEqual', function (values, value, number) {
+  if(!value && value!==0){ return true; }
+  return parseFloat(value) <= parseFloat(number);
+});
+
+var LatInput = React.createClass(_.extend({}, Formsy.Mixin, {
+  changeValue: function (event) {
+    this.setValue(event.currentTarget.value);
+  },
+  getDefaultProps: function(){
+    return {
+      validations:'isNumeric,isGreaterThanOrEqual:-85.05,isLessThanOrEqual:85.05',
+      validationErrors:{
+        isNumeric:'latitude must be a number',
+        isGreaterThanOrEqual:'latitude must be greater than -85.05',
+        isLessThanOrEqual:'latitude must be less than 85.05'
+      }
+    }
+  },
+  render: function () {
+    return (
+      <div className={this.showRequired() || this.showError() ? 'form-group has-error' : 'form-group'}>
+        <input type="text" className="form-control" placeholder="latitude" onChange={this.changeValue} value={this.getValue()}></input>
+        <span className="help-block">{this.getErrorMessage()}</span>
+      </div>
+    );
+  }
+}));
+
+var LngInput = React.createClass(_.extend({}, Formsy.Mixin, {
+  changeValue: function (event) {
+    this.setValue(event.currentTarget.value);
+  },
+  getDefaultProps: function(){
+    return {
+      validations:'isNumeric,isGreaterThanOrEqual:-180,isLessThanOrEqual:180',
+      validationErrors:{
+        isNumeric:'longitude must be a number',
+        isGreaterThanOrEqual:'longitude must be greater than -180',
+        isLessThanOrEqual:'longitude must be less than 180'
+      }
+    }
+  },
+  render: function () {
+    return (
+      <div className={this.showRequired() || this.showError() ? 'form-group has-error' : 'form-group'}>
+        <input type="text" className="form-control" placeholder="longitude" onChange={this.changeValue} value={this.getValue()}></input>
+        <span className="help-block">{this.getErrorMessage()}</span>
+      </div>
+    );
+  }
+}));
+
+module.exports = {
+  LatInput:LatInput,
+  LngInput:LngInput
+};
+
+/*
+function geocoding(searchTerm){
+  return new Promise(function(resolve, reject) {
+    var url = 'http://nominatim.openstreetmap.org/search.php?' + querystring.stringify({
+      q:searchTerm,
+      format:'jsonv2'
+    });
+    $.ajax(url, {error:reject, success:resolve});
+  });
+}
+
+function reverseGeocoding(lon, lat){
+  return new Promise(function(resolve, reject) {
+    var url = 'http://nominatim.openstreetmap.org/reverse?' + querystring.stringify({
+      format:'jsonv2',
+      lat:lat,
+      lon:lon
+    });
+    $.ajax(url, {error:reject, success:resolve});
+  });
+}
+*/
--- html/index.js
+++ html/index.js
@@ -3,51 +3,33 @@ var ReactDOM = require('react-dom');
 var $ = require('jquery');
 var _ = require('lodash');
 var querystring = require('querystring');
-var Alert = require('react-bootstrap').Alert;
-var Grid = require('react-bootstrap').Grid;
-var Row = require('react-bootstrap').Row;
-var Col = require('react-bootstrap').Col;
+var BS = require('react-bootstrap');
 var Map = require('react-leaflet').Map;
 var TileLayer = require('react-leaflet').TileLayer;
 var Promise = (window && window.Promise) || require('promise-js');
-
-function geocoding(searchTerm){
-  return new Promise(function(resolve, reject) {
-    var url = 'http://nominatim.openstreetmap.org/search.php?' + querystring.stringify({
-      q:searchTerm,
-      format:'jsonv2'
-    });
-    $.ajax(url, {error:reject, success:resolve});
-  });
-}
-
-function reverseGeocoding(lon, lat){
-  return new Promise(function(resolve, reject) {
-    var url = 'http://nominatim.openstreetmap.org/reverse?' + querystring.stringify({
-      format:'jsonv2',
-      lat:lat,
-      lon:lon
-    });
-    $.ajax(url, {error:reject, success:resolve});
-  });
-}
+var Geosuggest = require('react-geosuggest');
+var Forms = require('./forms');
 
 var ViewShed = React.createClass({
   render: function(){
     return (
-      <Grid fluid={true} style={{height:"100%"}}>
-        <Row>
-          <Col md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
-          <Col md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
-        </Row>
-        <Row style={{height:"100%"}}>
-          <Col md={12} style={{height:"100%"}}>
+      <BS.Grid fluid={true} style={{height:"100%"}}>
+        <BS.Row>
+          <BS.Col md={8}><Geosuggest /></BS.Col>
+          <BS.Col md={4}>
+            <Formsy.Form>
+              <Forms.LatInput name="lat"></Forms.LatInput>
+            </Formsy.Form>
+          </BS.Col>
+        </BS.Row>
+        <BS.Row style={{height:"100%"}}>
+          <BS.Col md={12} style={{height:"100%"}}>
             <Map center={[51.505, -0.09]} zoom={12} style={{height:"100%"}}>
               <TileLayer url='http://{s}.tile.osm.org/{z}/{x}/{y}.png' attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
             </Map>
-          </Col>
-        </Row>
-      </Grid>
+          </BS.Col>
+        </BS.Row>
+      </BS.Grid>
     )
   }
 });
@@ -57,8 +39,6 @@ $(function(){ReactDOM.render(<ViewShed/>, document.getElementById("application")
 //export some things to window
 _.extend(window || {}, {
   '_':_,
-  geocoding:geocoding,
   '$':$,
-  Promise:Promise,
-  reverseGeocoding:reverseGeocoding
+  Promise:Promise
 });
