a60cd4f1612d93afba9a8233c473fa9c66033fd6
Richard Klafter <richard@dronedeploy.com>
2015-12-28 16:47:30 -0700

    moved package.json to root

--- html/build.sh
+++ html/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-set -x
+set -o errexit
+set -o nounset
+set -o pipefail
+
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+cd $__dir
+
+set -x #log cmds
 rm bundle.*
 ./node_modules/browserify/bin/cmd.js -t [ babelify --presets [ react ] ] index.js -o bundle.js
 ./node_modules/node-sass/bin/node-sass ./index.scss > bundle.css
