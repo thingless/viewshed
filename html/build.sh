ac6e8903582ba0bc19b98660c7f4dcf1c467cc51
Richard Klafter <richard@dronedeploy.com>
2015-12-28 17:00:10 -0700

    fix build script

--- html/build.sh
+++ html/build.sh
@@ -5,9 +5,9 @@ set -o pipefail
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
-cd $__dir
 
 set -x #log cmds
-rm bundle.*
-./node_modules/browserify/bin/cmd.js -t [ babelify --presets [ react ] ] index.js -o bundle.js
-./node_modules/node-sass/bin/node-sass ./index.scss > bundle.css
+cd $__dir
+rm bundle.* | true
+../node_modules/browserify/bin/cmd.js -t [ babelify --presets [ react ] ] ./index.js -o ./bundle.js
+../node_modules/node-sass/bin/node-sass ./index.scss > ./bundle.css
