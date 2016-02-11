fda4406f69cfc6700b7e35ba9c0dfd0b9f09d0c5
FreeTheNation <rpklafter@yahoo.com>
2016-02-10 23:20:59 -0800

    uglify js

--- html/build.sh
+++ html/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -o errexit
-set -o nounset
 set -o pipefail
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+CI=${CI:-''}
 
 set -x #log cmds
 cd $__dir
 rm bundle.* || true
 ../node_modules/browserify/bin/cmd.js -t [ babelify --presets [ react ] ] ./index.js -o ./bundle.js
+[[ -n $CI ]] && ../node_modules/uglify-js/bin/uglifyjs ./bundle.js --screw-ie8 --output ./bundle.js
 ../node_modules/node-sass/bin/node-sass ./index.scss > ./bundle.css
