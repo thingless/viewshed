a0e18bbda11c7b40adee1d2f8ce898183ca1d9e3
freethenation <ftn@dyno>
2021-11-08 16:08:38 -0500

    make download script not re-download

--- download-data.sh
+++ download-data.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+set -e
 DOWNLOAD_URL="https://b2-free-egress-worker.mod64.workers.dev/cf-public/b03108db-65f2-4d7c-b884-bb908d111400/tiles.leveldb.tar.gz"
-wget "$DOWNLOAD_URL" -O - | tar -xz
+if [ -d "tiles.leveldb" ] 
+then
+    echo "tiles.leveldb already exsits" 
+else
+    echo "downloading tiles.leveldb..."
+    wget "$DOWNLOAD_URL" -O - | tar -xz
+fi
+
